67d4b1708d64f6e189758a8e58e94ed912ee27b4
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-16 12:28:15 +0100

    Setup

---
+++ main.py
@@ -0,0 +1,10 @@
+# Dictionnaries
+tech_dictionnary = {
+    "React": "Library for building user interfaces.",
+    "Node": "JavaScript runtime.",
+    "C#": "Strongly typed programming language",
+}
+
+# Retrieve data
+
+print(tech_dictionnary["Node"])
